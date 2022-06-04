3cf0d4340fd22e824055045d9fe196f3e201cda7
Yaroslav <32196737+accor-htt@users.noreply.github.com>
2022-06-04 18:04:06 +0700

    Add files via upload

---
+++ 4/1/www/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BOT_BASE_DIRECTORY', '/var/www');
+define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
+define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
+define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
+
+define('CALLBACK_API_CONFIRMATION_TOKEN', 'bb112c41'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('VK_API_ACCESS_TOKEN', 'daf52e374fa94ee010fc80388b1fd5cf582c850d93153eadf3fe48aa4d169a478882ea8965c0720a4e754'); //Ключ доступа сообщества
+define('YANDEX_API_KEY', '30e3213440-61233-1294-b3415-471212369886'); //Ключ для доступа к Yandex Speech Kit

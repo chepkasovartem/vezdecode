c7f93d0ddf018d66c9c6ad8af837ccdd47d71890
Kachalov <mrokiriko@yandex.ru>
2022-06-04 12:31:21 +0700

    remove comments in 1_sportprog_10

--- 1_sportprog/10/main.py
+++ 1_sportprog/10/main.py
@@ -21,7 +21,6 @@ prev_right = -1
 new_watch = []
 for i in range(n):
     (left, right) = (map(int, input().split()))
-    # print('left', left, 'right', right, prev_right)
 
     hours_to_watch = right - left
 
@@ -32,30 +31,17 @@ for i in range(n):
         watches.append(new_watch)
         new_watch = [hours_to_watch]
         prev_right = -1
-
 watches.append(new_watch)
-# print(watches)
-
-
-# x = 10
-# watches = [
-#     [9, 5, 2],
-#     [7]
-# ]
 
 max_watched_episodes = 0
 
 for watch in watches:
-    # print('watch', watch)
     m = len(watch)
 
     watch_sum = 0
 
     left = 0
     for right in range(0, m):
-        # print('subsum(watch, left, right)')
-        # print(watch, left, right)
-        # print(subsum(watch, left, right))
         if subsum(watch, left, right) <= x:
             episodes = (right - left + 1)
             max_watched_episodes = setmax(max_watched_episodes, episodes)
